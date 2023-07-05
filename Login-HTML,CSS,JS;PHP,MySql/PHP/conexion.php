332ad135d4db3034d24b9ea4cc3c8e80b9dbe076
WANDEXX <55203271+WJMD@users.noreply.github.com>
2023-07-05 12:18:13 -0400

    conexion a DB establecida con CleverCloud

--- Login-HTML,CSS,JS;PHP,MySql/PHP/conexion.php
+++ Login-HTML,CSS,JS;PHP,MySql/PHP/conexion.php
@@ -1,11 +1,15 @@
 <?php
+$host = "bzujvkqhvdhxbtqgbcez-mysql.services.clever-cloud.com";
+$user = "ufqmgf467v7btpob";
+$password = "xuNrXV8TMND3N3QYaTyU";
+$database = "bzujvkqhvdhxbtqgbcez";
+$port = 3306;
 
-$conexion = mysqli_connect("localhost","root","","login_register_db");
+$conexion = mysqli_connect($host, $user, $password, $database, $port);
 
-if($conexion){
-    echo 'conectado a la base de datos';
-}else{
-    echo 'no se conecto a la base de datos';
+if ($conexion) {
+    echo 'Conectado a la base de datos';
+} else {
+    echo 'No se pudo conectar a la base de datos';
 }
-
 ?>
