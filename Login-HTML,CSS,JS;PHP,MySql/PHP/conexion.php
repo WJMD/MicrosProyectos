7c874523ce52f239d679206bab496158f4ebd0ff
WANDEXX <55203271+WJMD@users.noreply.github.com>
2023-07-05 10:31:04 -0400

    modificacion

--- Login-HTML,CSS,JS;PHP,MySql/PHP/conexion.php
+++ Login-HTML,CSS,JS;PHP,MySql/PHP/conexion.php
@@ -1,7 +1,11 @@
 <?php
 
-$conexion = mysqli_connect("localhost","root","","login_register_db")
+$conexion = mysqli_connect("localhost","root","","login_register_db");
 
-
+if($conexion){
+    echo 'conectado a la base de datos';
+}else{
+    echo 'no se conecto a la base de datos';
+}
 
 ?>
